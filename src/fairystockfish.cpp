fa3c2f45dba1e1453be172a2c1ac701e3b641a67
Lakin Wecker <lakin.wecker@gmail.com>
2021-10-18 21:05:45 -0600

    Ported the rest of the pyfish methods, all of which seem appropriate.

--- src/fairystockfish.cpp
+++ src/fairystockfish.cpp
@@ -1,9 +1,71 @@
 #include "fairystockfish.h"
 
 #include <iostream>
+#include <climits>
+#include <sstream>
+
 #include "tabulate.hpp"
 
 namespace SF = Stockfish;
+
+bool fairystockfish::_fairystockfish_is_initialized;
+const int fairystockfish::VALUE_ZERO = 0;
+const int fairystockfish::VALUE_DRAW = 0;
+const int fairystockfish::VALUE_MATE = 32000;
+
+
+//------------------------------------------------------------------------------
+// This struct is an internal API intended to build a position from variant,
+// fen, a set of moves and whether or not it's chess960.
+//------------------------------------------------------------------------------
+struct PositionAndStates {
+    using MoveList = std::vector<std::string>;
+    std::unique_ptr<SF::Position> pos;
+    SF::StateListPtr states;
+
+    PositionAndStates(
+        std::string variant,
+        std::string fen,
+        MoveList const &moveList,
+        const bool isChess960
+    )
+        : pos{std::make_unique<SF::Position>()}
+        , states{SF::StateListPtr(new std::deque<SF::StateInfo>(1))}
+    {
+        // Initialize the variant.
+        const SF::Variant* v = SF::variants.find(std::string(variant))->second;
+        SF::UCI::init_variant(v);
+
+        // Figure out starting fen.
+        if (fen == "startpos") fen = v->startFen;
+
+        // Setup the position
+        pos->set(v, fen, isChess960, &states->back(), SF::Threads.main());
+
+        // Make the moves in the given position
+        for (auto moveStr : moveList) {
+            SF::Move m = SF::UCI::to_move(*pos, moveStr);
+            if (m == SF::MOVE_NONE) throw std::runtime_error("Invalid Move: '" + moveStr + "'");
+            // do the move
+            states->emplace_back();
+            pos->do_move(m, states->back());
+        }
+    }
+};
+
+Stockfish::Notation fairystockfish::fromOurNotation(Notation n) {
+    switch (n) {
+        case NOTATION_DEFAULT: return SF::NOTATION_DEFAULT;
+        case NOTATION_SAN: return SF::NOTATION_SAN;
+        case NOTATION_LAN: return SF::NOTATION_LAN;
+        case NOTATION_SHOGI_HOSKING: return SF::NOTATION_SHOGI_HOSKING;
+        case NOTATION_SHOGI_HODGES: return SF::NOTATION_SHOGI_HODGES;
+        case NOTATION_SHOGI_HODGES_NUMBER: return SF::NOTATION_SHOGI_HODGES_NUMBER;
+        case NOTATION_JANGGI: return SF::NOTATION_JANGGI;
+        case NOTATION_XIANGQI_WXF: return SF::NOTATION_XIANGQI_WXF;
+    }
+    return SF::NOTATION_DEFAULT;
+}
 
 fairystockfish::PieceInfo::PieceInfo()
     : pieceType{static_cast<SF::PieceType>(0)}
@@ -26,6 +88,10 @@ int fairystockfish::PieceInfo::id() const {
 }
 
 void fairystockfish::init() {
+    if (_fairystockfish_is_initialized) {
+        return;
+    }
+    _fairystockfish_is_initialized = true;
 
     // initialize stockfish
     SF::pieceMap.init();
@@ -39,6 +105,9 @@ void fairystockfish::init() {
     SF::Threads.set(SF::Options["Threads"]);
     SF::Search::clear(); // After threads are up
 }
+
+// TODO: make it so that the version number comes from compile time settings.
+std::string fairystockfish::version() { return "v0.0.1"; }
 
 void fairystockfish::info() {
     // Now print out some information
@@ -58,6 +127,17 @@ void fairystockfish::info() {
     }
 }
 
+void fairystockfish::setUCIOption(std::string name, std::string value) {
+    if (SF::Options.count(name)) SF::Options[name] = value;
+    else throw std::runtime_error("Unrecognized option");
+}
+
+void fairystockfish::loadVariantConfig(std::string config) {
+    std::stringstream ss(config);
+    SF::variants.parse_istream<false>(ss);
+    SF::Options["UCI_Variant"].set_combo(SF::variants.get_keys());
+}
+
 std::vector<std::string> fairystockfish::availableVariants() {
     return SF::variants.get_keys();
 }
@@ -72,4 +152,159 @@ std::map<std::string, fairystockfish::PieceInfo> fairystockfish::availablePieces
         retVal[info->name] = PieceInfo(id);
     }
     return retVal;
+}
+
+std::string fairystockfish::getSAN(
+    std::string variantName,
+    std::string fen,
+    std::string uciMove,
+    bool isChess960,
+    Notation ourNotation
+) {
+    // Can reuse the method below with a single move in the move list.
+    return getSANMoves(variantName, fen, {uciMove}, isChess960, ourNotation)[0];
+}
+
+std::vector<std::string> fairystockfish::getSANMoves(
+    std::string variantName,
+    std::string fen,
+    std::vector<std::string> uciMoves,
+    bool isChess960,
+    Notation ourNotation
+) {
+    SF::Notation notation = fromOurNotation(ourNotation);
+    if (notation == SF::NOTATION_DEFAULT)
+        notation = SF::default_notation(SF::variants.find(variantName)->second);
+
+    PositionAndStates posAndStates(variantName, fen, {}, isChess960);
+
+    std::vector<std::string> retVal;
+    for (auto uciMove : uciMoves) {
+
+        SF::Move m = SF::UCI::to_move(*posAndStates.pos, uciMove);
+        if (m == SF::MOVE_NONE) throw std::runtime_error("Invalid Move: '" + uciMove + "'");
+
+        retVal.push_back(SF::SAN::move_to_san(*posAndStates.pos, m, notation));
+
+        posAndStates.states->emplace_back();
+        posAndStates.pos->do_move(m, posAndStates.states->back());
+
+    }
+    return retVal;
+}
+
+std::vector<std::string> fairystockfish::getLegalMoves(
+    std::string variantName,
+    std::string fen,
+    std::vector<std::string> uciMoves,
+    bool isChess960
+) {
+    PositionAndStates posAndStates(variantName, fen, uciMoves, isChess960);
+
+    std::vector<std::string> legalMoves;
+    for (auto const &m : SF::MoveList<SF::LEGAL>(*posAndStates.pos)) {
+        legalMoves.push_back(SF::UCI::move(*posAndStates.pos, m));
+    }
+
+    return legalMoves;
+}
+
+std::string fairystockfish::getFEN(
+    std::string variantName,
+    std::string fen,
+    std::vector<std::string> uciMoves,
+    bool isChess960,
+    bool sFen,
+    bool showPromoted,
+    int countStarted
+) {
+    countStarted = std::min<unsigned int>(countStarted, INT_MAX); // pseudo-unsigned
+
+    PositionAndStates posAndStates(variantName, fen, uciMoves, isChess960);
+    return posAndStates.pos->fen(sFen, showPromoted, countStarted);
+}
+
+bool fairystockfish::givesCheck(
+    std::string variantName,
+    std::string fen,
+    std::vector<std::string> uciMoves,
+    bool isChess960
+) {
+    PositionAndStates posAndStates(variantName, fen, uciMoves, isChess960);
+    return posAndStates.pos->checkers() ? true : false;
+}
+
+int gameResult(
+    std::string variantName,
+    std::string fen,
+    std::vector<std::string> uciMoves,
+    bool isChess960=false
+) {
+    PositionAndStates posAndStates(variantName, fen, uciMoves, isChess960);
+    assert(!SF::MoveList<SF::LEGAL>(*posAndStates.pos).size());
+    SF::Value result;
+    bool gameEnd = posAndStates.pos->is_immediate_game_end(result);
+    if (!gameEnd)
+        result =
+            posAndStates.pos->checkers()
+            ? posAndStates.pos->checkmate_value()
+            : posAndStates.pos->stalemate_value();
+    return int(result);
+}
+
+std::tuple<bool, int> fairystockfish::isImmediateGameEnd(
+    std::string variantName,
+    std::string fen,
+    std::vector<std::string> uciMoves,
+    bool isChess960
+) {
+    PositionAndStates posAndStates(variantName, fen, uciMoves, isChess960);
+
+    SF::Value result;
+    bool gameEnd = posAndStates.pos->is_immediate_game_end(result);
+    return std::make_tuple(gameEnd, int(result));
+}
+
+std::tuple<bool, int> fairystockfish::isOptionalGameEnd(
+    std::string variantName,
+    std::string fen,
+    std::vector<std::string> uciMoves,
+    bool isChess960,
+    int countStarted
+) {
+    countStarted = std::min<unsigned int>(countStarted, INT_MAX); // pseudo-unsigned
+
+    PositionAndStates posAndStates(variantName, fen, uciMoves, isChess960);
+
+    SF::Value result;
+    bool gameEnd = posAndStates.pos->is_optional_game_end(result, 0, countStarted);
+
+    gameEnd = posAndStates.pos->is_optional_game_end(result, 0, countStarted);
+    return std::make_tuple(gameEnd, int(result));
+}
+
+std::tuple<bool, bool> fairystockfish::hasInsufficientMaterial(
+    std::string variantName,
+    std::string fen,
+    std::vector<std::string> uciMoves,
+    bool isChess960
+) {
+    PositionAndStates posAndStates(variantName, fen, uciMoves, isChess960);
+
+    bool wInsufficient = SF::has_insufficient_material(SF::WHITE, *posAndStates.pos);
+    bool bInsufficient = SF::has_insufficient_material(SF::BLACK, *posAndStates.pos);
+    return std::make_tuple(wInsufficient, bInsufficient);
+}
+
+bool fairystockfish::validateFEN(
+    std::string variantName,
+    std::string fen,
+    bool isChess960
+) {
+
+    return SF::FEN::validate_fen(
+        fen,
+        SF::variants.find(variantName)->second,
+        isChess960
+    );
 }
--- src/fairystockfish.h
+++ src/fairystockfish.h
@@ -21,9 +21,29 @@
 #include <map>
 
 namespace fairystockfish {
-    using Value = Stockfish::Value;
-    using Notation = Stockfish::Notation;
     using FenValidation = Stockfish::FEN::FenValidation;
+
+
+    // Copied from the apiutil.h
+    enum Notation {
+        NOTATION_DEFAULT,
+        // https://en.wikipedia.org/wiki/Algebraic_notation_(chess)
+        NOTATION_SAN,
+        NOTATION_LAN,
+        // https://en.wikipedia.org/wiki/Shogi_notation#Western_notation
+        NOTATION_SHOGI_HOSKING, // Examples: P76, S’34
+        NOTATION_SHOGI_HODGES, // Examples: P-7f, S*3d
+        NOTATION_SHOGI_HODGES_NUMBER, // Examples: P-76, S*34
+        // http://www.janggi.pl/janggi-notation/
+        NOTATION_JANGGI,
+        // https://en.wikipedia.org/wiki/Xiangqi#Notation
+        NOTATION_XIANGQI_WXF,
+    };
+    extern const int VALUE_ZERO;
+    extern const int VALUE_DRAW;
+    extern const int VALUE_MATE;
+
+    Stockfish::Notation fromOurNotation(Notation n);
 
     struct PieceInfo {
         private:
@@ -39,16 +59,256 @@ namespace fairystockfish {
             std::string betza() const;
     };
 
+    extern bool _fairystockfish_is_initialized;
+    ///------------------------------------------------------------------------------
+    /// Initialize the fairystockfish library.
+    ///------------------------------------------------------------------------------
     void init();
+
+    ///------------------------------------------------------------------------------
+    /// Return the version of the library.
+    ///
+    /// returns a string version number
+    ///------------------------------------------------------------------------------
+    std::string version();
+
+    ///------------------------------------------------------------------------------
+    /// Print to stdout useful information about the library and enabled variants
+    ///------------------------------------------------------------------------------
     void info();
 
+    // pyffish methods
+
+    ///------------------------------------------------------------------------------
+    /// Sets one of the UCI options that fairy stockfish supports.
+    ///
+    /// @param name The name of the parameter to set.
+    /// @param value The value of the parameters (in string form)
+    ///------------------------------------------------------------------------------
+    void setUCIOption(std::string name, std::string value);
+
+    ///------------------------------------------------------------------------------
+    /// Given a string containing .ini style configuration of variants, load them into
+    /// the supported variants for Fairy Stockfish.
+    ///
+    /// @param config A string containing the ini style variant configuration. Please
+    ///               see https://github.com/ianfab/Fairy-Stockfish/blob/master/src/variants.ini
+    ///               for example of syntax.
+    ///------------------------------------------------------------------------------
+    void loadVariantConfig(std::string config);
+
+    ///------------------------------------------------------------------------------
+    /// Returns the list of names of supported variants.
+    ///
+    /// @return A vector of variant names
+    ///------------------------------------------------------------------------------
     std::vector<std::string> availableVariants();
-    // info about variants
-    // Note that this is unsafe and does not check
-    // to ensure the variant name exists.
+
+    ///------------------------------------------------------------------------------
+    /// Returns the initial FEN for a given variant name. Note that this method does
+    /// not check that the variant you provided is within the set of supported
+    /// variants.
+    ///
+    /// @param variantName The name of the supported variant.
+    ///
+    /// @return A string representing the starting FNE for this variant.
+    ///------------------------------------------------------------------------------
     std::string initialFen(std::string variantName);
 
+    ///------------------------------------------------------------------------------
+    /// Returns a map from the name of a piece to information about that piece.
+    ///
+    /// @return The map
+    ///------------------------------------------------------------------------------
     std::map<std::string, PieceInfo> availablePieces();
+
+    ///------------------------------------------------------------------------------
+    /// Converts a UCI move into a SAN notation move given the variant and fen and
+    /// whether it's chess960 or not.
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param uciMove The move in UCI notation.
+    /// @param isChess960 Whether the game is chess960 or not.
+    /// @param notation The desired SAN notation.
+    ///
+    /// @return The move in SAN notation
+    ///------------------------------------------------------------------------------
+    std::string getSAN(
+        std::string variantName,
+        std::string fen,
+        std::string uciMove,
+        bool isChess960=false,
+        Notation notation=Notation::NOTATION_DEFAULT
+    );
+
+    ///------------------------------------------------------------------------------
+    /// Converts a set of UCI moves to SAN notation given the variant and fen and
+    /// whether it's chess960 or not.
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param uciMoves A vector of moves in UCI notation
+    /// @param isChess960 Whether the game is chess960 or not.
+    /// @param notation The desired SAN notation.
+    ///
+    /// @return A vector of moves in SAN notation
+    ///------------------------------------------------------------------------------
+    std::vector<std::string> getSANMoves(
+        std::string variantName,
+        std::string fen,
+        std::vector<std::string> uciMoves,
+        bool isChess960=false,
+        Notation notation=Notation::NOTATION_DEFAULT
+    );
+
+    ///------------------------------------------------------------------------------
+    /// Get legal moves from a given FEN and move list.
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param uciMoves A vector of moves in UCI notation
+    /// @param isChess960 Whether the game is chess960 or not.
+    ///
+    /// @return a vector of legal moves in UCI notation
+    ///------------------------------------------------------------------------------
+    std::vector<std::string> getLegalMoves(
+        std::string variantName,
+        std::string fen,
+        std::vector<std::string> uciMoves,
+        bool isChess960=false
+    );
+
+    ///------------------------------------------------------------------------------
+    /// Get the resulting FEN from a given FEN and move list
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param uciMoves A vector of moves in UCI notation
+    /// @param isChess960 Whether the game is chess960 or not.
+    /// @param sFen Whether the output is in sFen notation? (I'm guessing here)
+    /// @param showPromoted Whether the fen includes promoted pieces (I'm guessing here)
+    /// @param countStarted No clue. (probably has to do with games that end by certain
+    ///                     types of counts, like repetitions?)
+    ///
+    /// @return the FEN of the new position
+    ///------------------------------------------------------------------------------
+    std::string getFEN(
+        std::string variantName,
+        std::string fen,
+        std::vector<std::string> uciMoves,
+        bool isChess960=false,
+        bool sFen=false,
+        bool showPromoted=false,
+        int countStarted=0
+    );
+
+    ///------------------------------------------------------------------------------
+    /// Get check status from a given fen and movelist.
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param uciMoves A vector of moves in UCI notation
+    /// @param isChess960 Whether the game is chess960 or not.
+    ///
+    /// @return the FEN of the new position
+    ///------------------------------------------------------------------------------
+    bool givesCheck(
+        std::string variantName,
+        std::string fen,
+        std::vector<std::string> uciMoves,
+        bool isChess960=false
+    );
+
+    ///------------------------------------------------------------------------------
+    /// Gets result from a given FEN, considering variant end, checkmate and stalemate
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param uciMoves A vector of moves in UCI notation
+    /// @param isChess960 Whether the game is chess960 or not.
+    ///
+    /// @return Returns an integer that represents the result (don't know what it
+    ///         means yet
+    ///------------------------------------------------------------------------------
+    int gameResult(
+        std::string variantName,
+        std::string fen,
+        std::vector<std::string> uciMoves,
+        bool isChess960=false
+    );
+
+    ///------------------------------------------------------------------------------
+    /// Figures out if variant rules immediately end the game
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param uciMoves A vector of moves in UCI notation
+    /// @param isChess960 Whether the game is chess960 or not.
+    ///
+    /// @return Returns a boolean indicating if the game is an immediate end as well
+    ///         as the integer result value.
+    ///------------------------------------------------------------------------------
+    std::tuple<bool, int> isImmediateGameEnd(
+        std::string variantName,
+        std::string fen,
+        std::vector<std::string> uciMoves,
+        bool isChess960=false
+    );
+
+    ///------------------------------------------------------------------------------
+    /// Get result from given FEN if rules enable game end by player.
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param uciMoves A vector of moves in UCI notation
+    /// @param isChess960 Whether the game is chess960 or not.
+    /// @param countStarted No clue. (probably has to do with games that end by certain
+    ///                     types of counts, like repetitions?)
+    ///
+    /// @return Returns a boolean indicating if the game is an immediate end as well
+    ///         as the integer result value.
+    ///------------------------------------------------------------------------------
+    std::tuple<bool, int> isOptionalGameEnd(
+        std::string variantName,
+        std::string fen,
+        std::vector<std::string> uciMoves,
+        bool isChess960=false,
+        int countStarted=0
+    );
+
+    ///------------------------------------------------------------------------------
+    /// Checks for insufficient material on behalf of both players.
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param uciMoves A vector of moves in UCI notation
+    /// @param isChess960 Whether the game is chess960 or not.
+    ///
+    /// @return Returns two booleans, one for each player which indicate if that player
+    ///         has sufficient material
+    ///------------------------------------------------------------------------------
+    std::tuple<bool, bool> hasInsufficientMaterial(
+        std::string variantName,
+        std::string fen,
+        std::vector<std::string> uciMoves,
+        bool isChess960=false
+    );
+
+    ///------------------------------------------------------------------------------
+    /// Validates an input FEN.
+    ///
+    /// @param variantName The variant for the fen
+    /// @param fen The FEN of the current possition
+    /// @param isChess960 Whether the game is chess960 or not.
+    ///
+    /// @return Whether the FEN is valid or not.
+    ///------------------------------------------------------------------------------
+    bool validateFEN(
+        std::string variantName,
+        std::string fen,
+        bool isChess960=false
+    );
 }
 
 #endif // FAIRYSTOCKFISH_H
