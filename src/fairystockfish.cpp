73b0b55561262588a036daae9ef9de6a789465c0
Lakin Wecker <lakin@structuredabstraction.com>
2021-12-04 07:12:59 -0700

    Let's consider this v0.0.2

--- src/fairystockfish.cpp
+++ src/fairystockfish.cpp
@@ -134,7 +134,7 @@ void fairystockfish::init() {
 }
 
 // TODO: make it so that the version number comes from compile time settings.
-std::string fairystockfish::version() { return "v0.0.1"; }
+std::string fairystockfish::version() { return "v0.0.2"; }
 
 void fairystockfish::info() {
     // Now print out some information
